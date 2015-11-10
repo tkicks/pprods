c09aa2a1bddb92ba1f75c0812ef1dcb1cec8608d
Tyler <tkicks32@gmail.com>
2015-11-09 22:46:47 -0500

    quickpush

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 $(document).ready(function() {
 	var move_lets = $('.move_letters'),
-		playState = '-webkit-animation-play-state';
+		playState = '-webkit-animation-play-state',
+		scrollCount = 0;
 
-	move_lets.click(function() {
-		move_lets.css(playState, function(i, v) {
-			return v === 'paused' ? 'running' : 'paused';
-		});
-		$('body').toggleClass('paused', $(this).css(playState) === 'paused');
-	});
+	$(document).keydown(function (key) {
+    	switch (parseInt(key.which, 10)) {
+
+        // back slide
+        case 37:
+            scrollCount = scrollCount - 1;
+            console.log(scrollCount);
+            break;
+
+        // next slide
+        case 39:
+            scrollCount = scrollCount + 1;
+            console.log(scrollCount);
+            if (scrollCount === 6) {
+				move_lets.css(playState, function(i, v) {
+					return v === 'paused' ? 'running' : 'paused';
+				});
+				$('body').toggleClass('paused', $(this).css(playState) === 'paused');
+			};
+            break;
+
+        default:
+            break;
+        }
+    });
+
+	// move_lets.click(function() {
+
 });
