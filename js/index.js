f01d8b5ed94e70359cff8871b1d302921f6786d7
Tyler Kickham <tkicks32@gmail.com>
2017-06-23 16:00:03 -0400

    quick basic hardcoded

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
+var descIndeces = ["calBell", "grandBell", "JoeE", "jalMild"];
+var descriptions = ["Proof of concept", "shouldn't display"];
 
+function showDesc(whichDesc) {
+	console.log(whichDesc);
+	var index = descIndeces.indexOf(whichDesc);
+	console.log(descriptions[index]);
+	document.getElementById("calBellDesc").text = descriptions[index];
+	document.getElementById("calBellDesc").style.visibility = "visible";
+}
+
+function hideDesc() {
+	document.getElementById("calBellDesc").textContent = "";
+	document.getElementById("calBellDesc").style.visibility = hidden;
+}
